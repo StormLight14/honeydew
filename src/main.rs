22d2206b09a0a0c4e02837ddf2e9def9d155324c
StormLight14 <stormpower44@protonmail.com>
2024-03-20 14:52:31 -0600

    add commands!

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use futures::prelude::*;
 use irc::client::prelude::*;
 use std::io;
+use std::process;
 use std::thread;
 
 const SERVER: &'static str = "chat.freenode.net";
 
 #[tokio::main]
 async fn main() -> irc::error::Result<()> {
+    /*
     println!("What channel in {} would you like to talk in?", SERVER);
     let mut channel = String::new();
 
@@ -14,8 +16,9 @@ async fn main() -> irc::error::Result<()> {
         .read_line(&mut channel)
         .expect("Could not read input");
     let channel = channel.trim().to_string();
-
+    */
     println!("What nickname would you like to use?");
+    let channel = String::from("#rust-spam");
     let mut nickname = String::new();
 
     io::stdin()
@@ -41,8 +44,7 @@ async fn main() -> irc::error::Result<()> {
     });
 
     while let Some(message) = stream.next().await.transpose()? {
-        println!("{}", message);
-        println!("{}", parse_message(&message, &nickname.to_string()));
+        print!("{}", parse_message(&message, &nickname.to_string()));
     }
 
     Ok(())
@@ -51,33 +53,45 @@ async fn main() -> irc::error::Result<()> {
 fn parse_message(message: &Message, nickname: &String) -> String {
     let command = &message.command;
     let message_sender = &message.source_nickname();
+    //println!("COMMAND: {:?}", command);
 
-    if let Some(message_sender) = message_sender {
-        match command {
-            Command::PRIVMSG(sent_to, msg_text) => {
+    match command {
+        Command::PRIVMSG(sent_to, msg_text) => {
+            if let Some(message_sender) = message_sender {
                 if sent_to == nickname.trim() {
-                    format!("PM from {}: {}", message_sender, msg_text)
+                    format!("PM from {}: {}\n", message_sender, msg_text)
                 } else {
-                    format!("{}: {}", message_sender, msg_text)
+                    format!("{}: {}\n", message_sender, msg_text)
                 }
+            } else {
+                String::new()
             }
-            Command::NOTICE(_sent_to, notice_text) => {
-                format!("NOTICE: {}", notice_text)
-            }
-            Command::MOTD(motd) => {
-                if let Some(motd) = motd {
-                    format!("MOTD: {}", motd)
-                } else {
-                    String::from("No MOTD.")
-                }
-            }
-            Command::QUIT(_) => {
-                format!("")
-            }
-            _ => format!("{:?}", message),
         }
-    } else {
-        String::new()
+        Command::NOTICE(_sent_to, notice_text) => {
+            format!("NOTICE: {}\n", notice_text)
+        }
+        Command::Response(_response_type, text_vec) => {
+            format!("{}\n", text_vec.last().unwrap())
+        }
+        Command::MOTD(motd) => {
+            if let Some(motd) = motd {
+                format!("MOTD: {}\n", motd)
+            } else {
+                String::from("No MOTD?\n")
+            }
+        }
+        Command::PING(_, _) => String::new(),
+        Command::JOIN(_, _, _) => {
+            if let Some(message_sender) = message_sender {
+                format!("{} joined.\n", message_sender)
+            } else {
+                format!("Someone joined. Don't ask me who.\n")
+            }
+        }
+        Command::QUIT(_) => {
+            format!("")
+        }
+        _ => format!("{:?}", message),
     }
 }
 
@@ -90,6 +104,23 @@ fn send_message(client: Client, channel: &str) {
         let user_message = user_message.trim();
 
         if user_message.starts_with('/') {
+            let command = user_message.replacen("/", "", 1);
+            let mut command = command.split(' ');
+
+            if let Some(command) = command.nth(0) {
+                match command {
+                    "quit" => match client.send_quit("Closed by user.") {
+                        Ok(_) => {
+                            println!("Quitting.");
+                            process::exit(0);
+                        }
+                        Err(_) => {
+                            println!("Could not quit.");
+                        }
+                    },
+                    _ => {}
+                }
+            }
         } else {
             client
                 .send_privmsg(channel, user_message)
