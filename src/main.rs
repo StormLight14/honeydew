1eca48560cd0c617c4334c7e0fe93263a670652a
StormLight14 <stormpower44@protonmail.com>
2024-03-21 20:19:03 -0700

    add parting and better joining

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use colored::Colorize;
 use futures::prelude::*;
 use irc::client::prelude::*;
 use std::io;
@@ -5,6 +6,11 @@ use std::process;
 use std::thread;
 
 const SERVER: &'static str = "chat.freenode.net";
+
+#[derive(Debug)]
+enum CommandResult {
+    Join(String),
+}
 
 #[tokio::main]
 async fn main() -> irc::error::Result<()> {
@@ -40,7 +46,17 @@ async fn main() -> irc::error::Result<()> {
     let _sender = client.sender();
 
     thread::spawn(move || {
-        send_message(client, &channel);
+        let mut active_channel = channel;
+        loop {
+            let command_result = send_message(&client, &active_channel);
+            if let Some(command_result) = command_result {
+                match command_result {
+                    CommandResult::Join(channel_name) => {
+                        active_channel = channel_name;
+                    }
+                }
+            }
+        }
     });
 
     while let Some(message) = stream.next().await.transpose()? {
@@ -61,14 +77,14 @@ fn parse_message(message: &Message, nickname: &String) -> String {
                 if sent_to == nickname.trim() {
                     format!("PM from {}: {}\n", message_sender, msg_text)
                 } else {
-                    format!("{}: {}\n", message_sender, msg_text)
+                    format!("[{}] {}: {}\n", sent_to, message_sender, msg_text)
                 }
             } else {
                 String::new()
             }
         }
         Command::NOTICE(_sent_to, notice_text) => {
-            format!("NOTICE: {}\n", notice_text)
+            format!("{} {}\n", "NOTICE:".yellow(), notice_text)
         }
         Command::Response(_response_type, text_vec) => {
             format!("{}\n", text_vec.last().unwrap())
@@ -88,43 +104,71 @@ fn parse_message(message: &Message, nickname: &String) -> String {
                 format!("Someone joined. Don't ask me who.\n")
             }
         }
+        Command::PART(_, _) => {
+            if let Some(message_sender) = message_sender {
+                format!("{} left the channel.", message_sender.blue())
+            } else {
+                format!("")
+            }
+        }
         Command::QUIT(_) => {
             format!("")
         }
-        _ => format!("{:?}", message),
+        _ => format!("{:?}\n", message),
     }
 }
 
-fn send_message(client: Client, channel: &str) {
-    loop {
-        let mut user_message = String::new();
-        io::stdin()
-            .read_line(&mut user_message)
-            .expect("Could not read user input.");
-        let user_message = user_message.trim();
+fn send_message<'a>(client: &Client, channel: &str) -> Option<CommandResult> {
+    let mut user_message = String::new();
+    io::stdin()
+        .read_line(&mut user_message)
+        .expect("Could not read user input.");
+    let user_message = user_message.trim();
 
-        if user_message.starts_with('/') {
-            let command = user_message.replacen("/", "", 1);
-            let mut command = command.split(' ');
+    if user_message.starts_with('/') {
+        let command = user_message.replacen("/", "", 1);
+        let command = command
+            .split(' ')
+            .map(str::to_string)
+            .collect::<Vec<String>>();
 
-            if let Some(command) = command.nth(0) {
-                match command {
-                    "quit" => match client.send_quit("Closed by user.") {
-                        Ok(_) => {
-                            println!("Quitting.");
-                            process::exit(0);
-                        }
-                        Err(_) => {
-                            println!("Could not quit.");
-                        }
-                    },
-                    _ => {}
+        match command[0].as_str() {
+            "quit" => match client.send_quit("Closed by user.") {
+                Ok(_) => {
+                    println!("Quitting.");
+                    process::exit(0);
                 }
-            }
-        } else {
-            client
-                .send_privmsg(channel, user_message)
-                .expect("Message failed to send.");
+                Err(_) => {
+                    println!("Could not quit.");
+                }
+            },
+            "join" => match client.send_join(&command[1]) {
+                // /command channel_to_join part_other_channels (true/false, optional)
+                Ok(_) => {
+                    println!("Should have joined channel {}", &command[1].green());
+                    return Some(CommandResult::Join(command[1].to_string()));
+                }
+                Err(_) => {
+                    println!("{} {}.", "Error joining channel".red(), &command[1].green());
+                }
+            },
+            "part" => match client.send_part(&command[1]) {
+                Ok(_) => println!("Should have parted from channel {}", &command[1].green()),
+                Err(_) => {
+                    println!(
+                        "{} {}.",
+                        "Error occured joining channel".red(),
+                        &command[1].green()
+                    )
+                }
+            },
+            _ => {}
         }
+    } else {
+        client
+            .send_privmsg(channel, user_message)
+            .expect("Message failed to send.");
     }
+
+    None
 }
